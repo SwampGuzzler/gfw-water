91b940ffdcbcede77f6916aa2ecd2021b9f52d1d
Lucas Cotner <lc07@uw.edu>
2016-07-08 10:26:31 -0400

    Configured intern functional tests to run in browser via Selenium Server

--- tests/intern.js
+++ tests/intern.js
@@ -6,23 +6,28 @@ define({
 	// specified browser environments in the `environments` array below as well. See
 	// <https://theintern.github.io/intern/#option-capabilities> for links to the different capabilities options for
 	// different services.
-	//
+
+	proxyPort: 9000,
+
+	// A fully qualified URL to the Intern proxy
+	proxyUrl: 'http://localhost:9000/',
+
 	// Note that the `build` capability will be filled in with the current commit ID or build tag from the CI
 	// environment automatically
 	capabilities: {
-		'browserstack.selenium_version': '2.45.0'
+		'browserstack.selenium_version': '2.46.0'
 	},
 
 	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
-		{ browserName: 'internet explorer', version: '11', platform: 'WIN8' },
-		{ browserName: 'internet explorer', version: '10', platform: 'WIN8' },
-		{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
-		{ browserName: 'firefox', version: '37', platform: [ 'WINDOWS', 'MAC' ] },
-		{ browserName: 'chrome', version: '39', platform: [ 'WINDOWS', 'MAC' ] },
-		{ browserName: 'safari', version: '8', platform: 'MAC' }
+		// { browserName: 'internet explorer', version: '11', platform: 'WIN8' },
+		// { browserName: 'internet explorer', version: '10', platform: 'WIN8' },
+		// { browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
+		// { browserName: 'firefox', version: '37', platform: [ 'WINDOWS', 'MAC' ] },
+		// { browserName: 'chrome', version: '39', platform: [ 'WINDOWS', 'MAC' ] },
+		{ browserName: 'chrome' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
@@ -30,7 +35,15 @@ define({
 
 	// Name of the tunnel class to use for WebDriver tests.
 	// See <https://theintern.github.io/intern/#option-tunnel> for built-in options
-	tunnel: 'BrowserStackTunnel',
+	tunnel: 'NullTunnel',
+
+	// Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
+	// in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
+	// publishing this configuration file somewhere
+	webdriver: {
+		host: 'localhost',
+		port: 4444
+	},
 
 	// Configuration options for the module loader; any AMD configuration options supported by the AMD loader in use
 	// can be used here.
@@ -58,7 +71,7 @@ define({
 	suites: ['tests/unit/appUtils', 'tests/unit/configTest', 'tests/unit/paramsTest'],
 
 	// Functional test suite(s) to execute against each browser once non-functional tests are completed
-	functionalSuites: [ /* 'myPackage/tests/functional' */ ],
+	functionalSuites: [ 'tests/functional/index' ],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:tests|node_modules|build\/vendor)\//
