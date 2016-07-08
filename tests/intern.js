d312a08c92e549c41e037ff202a4bb75227b480e
Lucas Cotner <lc07@uw.edu>
2016-07-08 11:23:35 -0400

    Updated tests platform

--- tests/intern.js
+++ tests/intern.js
@@ -27,7 +27,7 @@ define({
 		// { browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
 		// { browserName: 'firefox', version: '37', platform: [ 'WINDOWS', 'MAC' ] },
 		// { browserName: 'chrome', version: '39', platform: [ 'WINDOWS', 'MAC' ] },
-		{ browserName: 'chrome', version: '50' }
+		{ browserName: 'chrome', version: '50', platform: 'MAC' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
